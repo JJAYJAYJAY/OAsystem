951754e165359669acf15b08b194b3b5b0b24df6
JAYJAY <2422301985@qq.com>
2024-01-28 00:23:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<style>
+
+</style>
+<template>
+  <div>
+    {{msg}}
+  </div>
+  <div>
+    {{msg1}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      msg:'hello world',
+      msg1:'hello'
+    }
+  }
+}
+</script>
